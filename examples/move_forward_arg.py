cdc97d4da22714ba1fe0a611b44c1a180df2b041
postprandial <tamhahn@gmail.com>
2024-02-06 16:42:40 -0800

    new moveforward_arg file

---
+++ examples/move_forward_arg.py
@@ -0,0 +1,17 @@
+#edited from move_forward to measure how power translates into velocity
+
+import picar_4wd as fc
+import time
+import sys
+
+power = sys.argv[1]
+
+print(f"going forward with power: {power}")
+
+try:
+    while True:
+        fc.forward(power)
+        time.sleep(1)
+finally:
+    fc.stop()
+    time.sleep(0.2)
