760a5bdf2f7c279483176d60b232db15ff46c56c
postprandial <tamhahn@gmail.com>
2024-02-06 16:31:47 -0800

    added turn files from laptop to changes

---
+++ examples/turn.py
@@ -0,0 +1,20 @@
+import picar_4wd as fc
+import time, sys, select
+
+power = 50
+
+while True:
+    print(f"turning with {power} speed for 1 second: starting in 2 seconds")
+    time.sleep(2)
+    if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+        line = input()
+        break
+    fc.turn_right(power)
+    time.sleep(1)
+    fc.stop()
+    # power += 5
+    # if power == 40:
+    #    power = 5
+
+fc.stop()
+print("All done")
